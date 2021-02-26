01656b2e13f8934f30269616395c9866f886ad5d
mcapurri <m.capurri@gmail.com>
2021-02-26 10:46:39 +0100

    styling...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import CountriesList from './Components/CountriesList/CountriesList';
 import CountryDetails from './Components/CountryDetails/CountryDetails';
 
@@ -10,10 +10,21 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={CountriesList} />
-        <Route exact path="/countries/:cca3" component={CountryDetails} />
-      </Switch>
+      <div style={{ display: 'flex', width: '100vw', margin: '0 5% 5% 5%' }}>
+        <div style={{ width: '30%' }}>
+          <Route path="/" component={CountriesList} />
+        </div>
+        <div
+          style={{
+            width: '50%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Route path="/countries/:cca3" component={CountryDetails} />
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/CountryDetails/CountryDetails.js
+++ src/Components/CountryDetails/CountryDetails.js
@@ -5,10 +5,12 @@ const CountryDetails = (props) => {
   console.log('props', props.match);
   const countryId = props.match.params.cca3;
   const country = countries.find((country) => country.cca3 === countryId);
+  console.log('country', country);
   return (
     <div>
+      <h1 style={{ fontSize: '9rem' }}>{country.flag}</h1>
       <h2>Common name: {country.name.common}</h2>
-      <h3>Official name: {country.name.common}</h3>
+      <h3>Official name: {country.name.official}</h3>
       <h3>Capital: {country.capital}</h3>
       <h4>Languages: </h4>
       <h4>Currency: {country.currency}</h4>
